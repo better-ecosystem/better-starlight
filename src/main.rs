0e9d5566328d659778b5df1dac3782f419e955f3
SAN_DESH63 <99701679+Sandesh2007@users.noreply.github.com>
2025-07-10 20:03:39 +0545

    feat: added dummpy holder
    dummy holder for apps and other for testing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 pub mod utils;
+pub mod ui;
 
-use adw::prelude::AdwApplicationWindowExt;
 use clap::{Parser, ArgAction};
-use gtk::{gdk::Key, prelude::*, EventControllerKey};
+use gtk::prelude::*;
 use gtk4_layer_shell::{KeyboardMode, Layer, LayerShell};
 use lazy_static::lazy_static;
 
-use crate::utils::logger::{LogLevel, Logger};
+use crate::{ui::ui::build_main_ui, utils::logger::{LogLevel, Logger}};
 
 lazy_static! {
     static ref LOG: Logger = Logger::new("main",LogLevel::Debug);
@@ -21,7 +21,8 @@ struct Args{
     debug: bool,
 }
 
-fn main(){
+#[tokio::main]
+async fn main(){
 
     let args = Args::parse();
 
@@ -35,45 +36,22 @@ fn main(){
         LOG.error("Failed to initialize ADW");
         LOG.error(&format!("Error: {}", e));
     });
-
+    
+    LOG.debug("application initialized");
     let app = adw::Application::new(Some("com.btde.starlight"), Default::default());
 
-    app.connect_activate(build_ui);
+    LOG.debug("building ui");
+    app.connect_activate(|app| {
+        let window = build_main_ui(app);
+        setup_layer_shell(&window);
+        window.present();
+    });
+
+    LOG.debug("running main app");
     let args: Vec<String> = Vec::new();
     app.run_with_args(&args);
 }
 
-pub fn build_ui(app: &adw::Application){
-
-    let window = adw::ApplicationWindow::new(app);
-    window.set_title(Some("starlight"));
-    window.set_default_size(600, 600);
-
-    setup_layer_shell(&window);
-    LOG.debug("Window layer setup complete");
-
-    // Close app when presses ESCAPE button
-    let key_controller = EventControllerKey::new();
-    let window_clone = window.clone();
-    key_controller.connect_key_pressed(move |_controller , _key, _keycode , _state| match _key{
-        Key::Escape => {
-            LOG.debug("Application closed");
-            window_clone.close();
-            true.into()
-        }
-        _ => false.into(),
-    });
-    let search_entry = gtk::Entry::new();
-    search_entry.set_can_focus(false);
-    
-    let main_box = gtk::Box::new(gtk::Orientation::Horizontal, 0);
-    main_box.set_size_request(200, 400);
-    main_box.append(&search_entry);
-    window.set_content(Some(&main_box));
-    window.add_controller(key_controller);
-
-    window.present(); 
-}
 
 // Setup as a layershell rather than a normal window
 fn setup_layer_shell(window: &adw::ApplicationWindow) {
---
+++ src/ui/ui.rs
@@ -0,0 +1,112 @@
+use adw::{ApplicationWindow, prelude::AdwApplicationWindowExt};
+use gtk::{
+    gdk::Key, prelude::{BoxExt, EntryExt, GtkWindowExt, ListBoxRowExt, WidgetExt}, Box, EventControllerKey
+};
+use lazy_static::lazy_static;
+
+use crate::utils::logger::{LogLevel, Logger};
+
+lazy_static! {
+    static ref LOG: Logger = Logger::new("ui", LogLevel::Debug);
+}
+
+pub fn build_main_ui(app: &adw::Application) -> ApplicationWindow {
+    let window = adw::ApplicationWindow::new(app);
+    window.set_title(Some("starlight"));
+    window.set_size_request(600, 600);
+
+    LOG.debug("window layer setup complete");
+
+    // Close app when presses ESCAPE button
+    let key_controller = EventControllerKey::new();
+    let window_clone = window.clone();
+    key_controller.connect_key_pressed(move |_controller, _key, _keycode, _state| match _key {
+        Key::Escape => {
+            LOG.debug("application closed");
+            window_clone.close();
+            true.into()
+        }
+        _ => false.into(),
+    });
+
+    // mainbox that holds all components
+    let content = Box::new(gtk::Orientation::Vertical, 10);
+    content.set_margin_top(12);
+    content.set_margin_bottom(12);
+    content.set_margin_start(12);
+    content.set_margin_end(12);
+
+    // search entry
+    let search_entry = gtk::Entry::new();
+    search_entry.set_placeholder_text(Some("Search..."));
+    search_entry.set_icon_from_icon_name(
+        gtk::EntryIconPosition::Primary,
+        Some("system-search-symbolic"),
+    );
+    search_entry.add_css_class("flat");
+    search_entry.add_css_class("search-entry");
+    search_entry.set_hexpand(true);
+
+    // list box holding apps
+    let list_box = gtk::ListBox::new();
+    list_box.set_vexpand(true);
+    list_box.add_css_class("apps-list");
+
+    // just for test
+    for _ in 0..4 {
+        let row = create_app_row("App name", "description if have any");
+        list_box.append(&row);
+    }
+
+    content.append(&search_entry);
+    content.append(&list_box);
+
+    window.add_controller(key_controller);
+    window.set_content(Some(&content));
+    window.present();
+
+    window
+}
+
+
+fn create_app_row(name: &str, description: &str) -> gtk::ListBoxRow {
+    let row = gtk::ListBoxRow::new();
+    row.set_margin_top(4);
+    row.set_margin_bottom(4);
+    row.set_margin_start(8);
+    row.set_margin_end(8);
+
+
+    let row_box = Box::new(gtk::Orientation::Horizontal, 0);
+    row_box.set_margin_top(8);
+    row_box.set_margin_bottom(8);
+    row_box.set_margin_start(8);
+    row_box.set_margin_end(8);
+
+    // app icon
+    let icon = gtk::Image::from_icon_name("applications-system-symbolic");
+    icon.set_pixel_size(48);
+
+    // app name and description
+    let app_box = Box::new(gtk::Orientation::Vertical, 2);
+    app_box.set_margin_top(8);
+    app_box.set_margin_bottom(8);
+    app_box.set_margin_start(8);
+    app_box.set_margin_end(8);
+
+    let app_title = gtk::Label::new(Some(name));
+    app_title.add_css_class("title");
+
+    let app_desc = gtk::Label::new(Some(description));
+    app_desc.add_css_class("dim-label");
+
+    app_box.append(&app_title);
+    app_box.append(&app_desc);
+
+    row_box.append(&icon);
+    row_box.append(&app_box);
+
+    row.set_child(Some(&row_box));
+    row.add_css_class("card");
+    row
+}
